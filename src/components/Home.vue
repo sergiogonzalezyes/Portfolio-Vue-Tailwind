0d2b4d5dda582f4dce2bafed3a3a4bdc230f1466
Sergio Gonzalez <89554725+sergiogonzalezyes@users.noreply.github.com>
2024-02-20 08:03:06 -0600

    some more changes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,12 +4,12 @@
         <div class="lg:flex lg:flex-row">
             <div class="flex justify-center items-center"><img src="../assets/images/profilepic.jpg" alt="Profile Picture" class="rounded-full w-80 h-80 xs:h-60 xs:w-60 mt-4 mb-4 shadow-2xl"></div>
             <div class="flex flex-col justify-center items-center lg:mt-24 px-10">
-                <h1 class="text-5xl font-bold text-center">Sergio Gonzalez</h1>
+                <h1 class="text-5xl font-bold text-center mt-5">Sergio Gonzalez</h1>
 
-                <h2 class="text-3xl font-bold text-center mt-4 xs:text-xl">Full-Stack Developer</h2>
+                <h2 class="text-3xl font-bold text-center mt-6 xs:text-xl">Full-Stack Developer</h2>
                 
                 <!-- Social Media Links Icon buttons -->
-                <div class="flex justify-center items-center gap-6 mt-5">
+                <div class="flex justify-center items-center gap-6 mt-10">
                     <button aria-label="LinkedIn" class="transition duration-300 ease-in-out hover:scale-[1.3]">
                         <a href="https://www.linkedin.com/in/sergio-gonzalez-garcia-b5082a264/" target="_blank">
                             <i class="fa-brands fa-linkedin-in text-3xl cursor-pointer"></i>
@@ -30,7 +30,7 @@
         </div>
 
 
-        <div class="flex flex-col justify-center items-center mt-5 lg:w-3/5">
+        <div class="flex flex-col justify-center items-center mt-10 lg:w-3/5">
             <h5 class="text-2xl font-bold">About Me</h5>
             <!-- line break -->
             <div class="w-full h-0.5 bg-white mt-2 mb-2"></div>
