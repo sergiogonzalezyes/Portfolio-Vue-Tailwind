5594691daa99271904cd5dbddf7917f6e237718c
Sergio Gonzalez <89554725+sergiogonzalezyes@users.noreply.github.com>
2024-02-21 04:56:07 -0600

    sharpening the saw

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -9,8 +9,7 @@
 
                         <img src="../assets/images/lancium__logo__tagline__white__blue.png" alt="Lancium" class="w-80 h-15 my-4"/>
                         <p class="text-center">During my tenure at Lancium in Houston, TX, from May to December 2023, I contributed to the development of an internal application
-                             that enhanced database accessibility and operational efficiency using Python, SQLAlchemy, PostgreSQL, and Streamlit. As an Information Technology Intern,
-                              I further extended my contributions to include network infrastructure and cybersecurity, showcasing my adaptability and dedication to advancing company
+                             that enhanced database accessibility and operational efficiency using Python, SQLAlchemy, PostgreSQL, and Streamlit. I further extended my contributions to include network infrastructure and cybersecurity, showcasing my adaptability and dedication to advancing company
                                operations through both software development and IT support.
                         </p>
                     </div>
