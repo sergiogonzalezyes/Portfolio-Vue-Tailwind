fec0b0dcd00b2510c8dfd3a92f25f11dcb80fa46
Sergio Gonzalez <89554725+sergiogonzalezyes@users.noreply.github.com>
2024-02-21 03:34:20 -0600

    reordered footer components

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,10 +1,11 @@
 <template>
         <div class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6 justify-between text-center">
             <div class="flex-1"></div>
+            
+            <p class="sm:text-center flex-1">Created by Sergio Gonzalez</p>
             <div @click="scrollToTop" class="flex items-center hover:text-weather-secondary duration-150 cursor-pointer flex-1 justify-end">
                 <i class="fa-solid fa-chevron-up text-2xl"></i>
             </div>
-            <p class="sm:text-center flex-1">Created by Sergio Gonzalez</p>
         </div>
 </template>
 
